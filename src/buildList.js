2f8675555a19255e286000d35a9f076b4773e4ec
Vladimir Sushkov <volodya.sushkov@gmail.com>
2022-07-14 16:01:13 +0200

    Add Aurora Testnet tokens WETH, CGT, DAI, CSC, wCT1

--- src/buildList.js
+++ src/buildList.js
@@ -4,6 +4,7 @@ const mainnet = require('./tokens/mainnet.json');
 const skaleTestnetV2 = require('./tokens/skale-testnet-v2.json');
 const skaleMainnet = require('./tokens/skale-mainnet.json');
 const bobaRinkeby = require('./tokens/boba-rinkeby.json');
+const auroraTestnet = require('./tokens/aurora_testnet.json');
 
 module.exports = function buildList() {
   const parsed = version.split('.');
@@ -26,7 +27,8 @@ module.exports = function buildList() {
       ...mainnet,
       ...skaleTestnetV2,
       ...skaleMainnet,
-      ...bobaRinkeby
+      ...bobaRinkeby,
+      ...auroraTestnet
     ]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
